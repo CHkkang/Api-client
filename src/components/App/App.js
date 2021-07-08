e9ed1a0db400057ccb0666263343890979b25ede
강찬혁 <ch.kang@promptech.co.kr>
2021-07-08 19:06:21 +0900

    네비게이션 바 추가로 인한 router 수정

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import SignUpContainer from '../SignUp/SignUpContainer';
 import Login from '../Login/Login';
 import CreateProfileForm from '../Profile/CreateProfileForm';
 import Profile from '../Profile/Profile'
+import MenuAppBar from '../NavBar/MenuAppBar';
 
 // const Router = withRouter(Login);
 
 function App() {
   return (
-    <div>
-      <Route exact path="/" component={Login} />
-      <Route path="/signup" component={SignUpContainer}/>
-      <Route path="/profile/mypage" component={Profile}/>
-      <Route path="/profile/create" component={CreateProfileForm}/>
-    </div>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/signup" component={SignUpContainer} />
+          <Route path="/profile/create" component={CreateProfileForm}/>
+          <Route
+            exact path="*"
+            component={() => (
+              <div>
+                <MenuAppBar />
+                <Route path="/profile/mypage" component={Profile}/>
+              </div>
+            )}
+          />
+        </Switch>
   );  
 }
 
