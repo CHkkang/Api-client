dad29c759f3f727117855dc7128d69bb2f8f3df9
강찬혁 <ch.kang@promptech.co.kr>
2021-07-08 15:57:58 +0900

    formData Ojbect에 파일 담아줘서 request 날리는 기능 구현

--- src/components/Profile/CreateProfileForm.js
+++ src/components/Profile/CreateProfileForm.js
@@ -2,7 +2,15 @@ import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import {TextField, Link, Box, RadioGroup, Typography, Container, FormControlLabel, Radio} from '@material-ui/core';
+import {
+  TextField, 
+  Link, 
+  Box, 
+  Typography, 
+  Container, 
+  InputLabel,
+  MenuItem,
+} from '@material-ui/core';
 import Portrait from '@material-ui/icons/Portrait';
 import { makeStyles } from '@material-ui/core/styles';
 import { useForm } from "react-hook-form";
@@ -19,6 +27,16 @@ function Copyright() {
     </Typography>
   );
 }
+const currencies = [
+  {
+    value: 'male',
+    label: '남자',
+  },
+  {
+    value: 'female',
+    label: '여자',
+  },
+];
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -36,50 +54,63 @@ const useStyles = makeStyles((theme) => ({
     marginTop: theme.spacing(1),
   },
   submit: {
+    padding: '6px 16px',
     margin: theme.spacing(3, 0, 2),
   },
 }));
 
+
+function appendData(data, id) {
+  const formData = new FormData()
+  const input = document.getElementById('photo');
+  const file = input.files[0]
+  for(var key in data) {
+    if(key === "file"){
+      formData.append(key, file);
+    } else {
+      formData.append(key, data[key]);
+    }
+  }
+  formData.append("user_id", id)
+  for (var pair of formData.entries()) { console.log(pair[0]+ ', ' + pair[1] + ' type : '+ typeof pair[1]) };
+
+  return formData
+}
+
 async function createProfile(data, props) {
-    fetch('/api/profiles', {
-      method: 'POST',
-      headers: new Headers({
-        'Content-Type': 'multipart/form-data',
-        "Accept": "application/json",
-        "type": "formData"
-      }),
-      body: data
-    })
-      .then(data => data.json())
-      .then((result) => {
-           if(result.status === "Success") {
-              localStorage.setItem ("token", result.token);
-              props.history.push('/profile');
-            } else {
-              alert("아이디 및 패스워드가 일치하지 않습니다.")
-              props.history.push('/');
-            }
-       });
-    
-   }
+  const id = props.location.state.user_id
+  const formData = appendData(data, id)
+  fetch('/api/profiles', {
+    method: 'POST',
+    body: formData
+  })
+    .then(data => data.json())
+    .then((result) => {
+          if(result.status === "Success") {
+            localStorage.setItem ("token", result.token)
+            props.history.push({
+              pathname: '/profile/mypage',
+              state: {user_id: result.user_id}
+            });
+          } else {
+            alert("프로필 형식 오류!")
+          }
+      })
+}
 
 export default function CreateProfileForm(props) {
-  const classes = useStyles(); 
-  const [value, setValue] = React.useState("");
-  const [error, setError] = React.useState(false);
-  const [helperText, setHelperText] = React.useState("");
+  const classes = useStyles() 
+  const [currency, setCurrency] = React.useState('male');
+
+  const handleChange = (event) => {
+    setCurrency(event.target.value);
+  };
   
   const { register, handleSubmit } = useForm();
   const onSubmit = data => {
-    console.log("submit ")
     createProfile(data, props)
   };
 
-  const handleRadioChange = event => {
-    setValue(event.target.value);
-    setHelperText(" ");
-    setError(false);
-  };
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
@@ -91,100 +122,147 @@ export default function CreateProfileForm(props) {
           Profile
         </Typography>
         <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
+            <Box mb={1}>
+              <TextField
+                  {...register('name', {
+                  required: true
+                  })}
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="name"
+                  label="이름"
+                  name="name"
+                  autoComplete="on"
+                  autoFocus
+              />
+            </Box>
+            <InputLabel>
+                프로필사진
+            </InputLabel>
             <TextField
-                {...register('name', {
-                required: true
-                })}
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="name"
-                label="이름"
-                name="name"
-                autoComplete="on"
-                autoFocus
-            />
-            <TextField
-            {...register('file', {
-            required: true
-            })}
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="file"
-            label="프로필 사진"
-            name="file"
-            type="file"
-            autoComplete="on"
-            autoFocus
+              {...register('file', {
+              required: true
+              })}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="photo"
+              name="file"
+              type="file"
+              autoComplete="on"
+              autoFocus
+              inputProps={{ accept: 'image/*' }}
              />
-            <RadioGroup label="성별" aria-label="sex" name="sex" value={value} onChange={handleRadioChange}>
-              <FormControlLabel value="male" control={<Radio />} label="Male" />
-              <FormControlLabel value="female" control={<Radio />} label="Female" />
-           </RadioGroup>
-            <TextField
-                {...register('dob', {
+             <Box mb={1}>
+              <TextField
+                {...register('sex', {
                 required: true
                 })}
                 variant="outlined"
                 margin="normal"
+                id="sex"
                 required
-                fullWidth
-                name="dob"
-                id="dob"
-                type="date"
+                name="sex"
                 autoComplete="on"
+                select
+                label="성별"
+                value={currency}
+                onChange={handleChange}
+                fullWidth
+                autoFocus
+                >
+                {currencies.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Box>
+            <InputLabel>
+            생년월일
+            </InputLabel>
+            <TextField
+              {...register('dob', {
+              required: true
+              })}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="dob"
+              id="dob"
+              type="date"
+              autoComplete="on"
             />
             <TextField
-                {...register('address', {
-                required: true
-                })}
-                variant = "outlined"
-                margin="normal"
-                required
-                fullWidth
-                name="address"
-                label="주소"
-                id="address"
-                autoComplete="off"
+              {...register('address', {
+              required: true
+              })}
+              variant = "outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="address"
+              label="주소"
+              id="address"
+              autoComplete="off"
             />
             <TextField
-                {...register('job', {
-                required: true
-                })}
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                name="job"
-                label="직업"
-                id="job"
-                autoComplete="on"
+              {...register('email', {
+              required: true
+              })}
+              variant = "outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="email"
+              label="이메일"
+              type="email"
+              id="email"
+              autoComplete="off"
+              helperText="이메일 형식에 맞게 입력하세요"
             />
             <TextField
-                {...register('phone', {
-                required: true
-                })}
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                name="phone"
-                label="전화번호"
-                id="phone"
-                autoComplete="on"
+              {...register('job', {
+              required: true
+              })}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="job"
+              label="직업"
+              id="job"
+              autoComplete="on"
+            />
+            <TextField
+              {...register('phone', {
+              required: true
+              })}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="phone"
+              label="전화번호"
+              id="phone"
+              autoComplete="on"
+              inputProps={{ pattern: "[0-9]*" }}
+              helperText="숫자만 입력하세요"
             />
             <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="primary"
-                className={classes.submit}
-            >
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+          >
             Save
-            </Button>
+          </Button>
+            
         </form>
       </div>
       <Box mt={8}>
